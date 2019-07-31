e33f240706098d538d226f4d9602bfc2c21bfe2d
aloksoni0501 <aloksoni.lise@gmail.com>
2019-07-31 17:25:02 +0530

    hiiii alok

--- com/qa/selenium/WebDriverBasics.py
+++ com/qa/selenium/WebDriverBasics.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import time
 
 driver = webdriver.Chrome("/Users/macbookpro/PycharmProjects/PythonProjectAutomation/drivers/chromedriver 2")
 driver.get("https://www.facebook.com")
@@ -16,10 +17,13 @@ driver.find_element_by_id("u_0_x").send_keys("alok@123")
 Select(driver.find_element_by_xpath("//select[@id='day']")).select_by_value('1')
 Select(driver.find_element_by_xpath("//select[@id='month']")).select_by_index(1)
 Select(driver.find_element_by_xpath("//select[@id='year']")).select_by_value('1999')
+time.sleep(5)
+
 driver.find_element_by_xpath("//input[@id='u_0_6']").click()
+time.sleep(4)
 driver.find_element_by_xpath("//button[@id='u_0_15']").click()
 
 #assert "Facebook – log in or sign up" in title
 #driver.maximize_window()
 
-#driver.quit()
+driver.quit()
